d6cf262e65907407b9fc61a1d41bd0e3c50752c6
burn56 <matt.t.urbano@gmail.com>
2020-09-08 16:18:42 -0700

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -17,8 +17,7 @@ O365-MFAConnect (MFA O365 Connection)
 WanIP (Get Current Machine's WAN IP)
 Run-AsAdmin (Open a New Powershell Window as Admin)
 Get-LatestCoreProfile (Update the Profile with the latest version)
-Remove-Profile (To erase this profile from this Machine)
-Test"
+Remove-Profile (To erase this profile from this Machine)"
 function CoreAuditLogCheck{
 $AuditLogsEnabled = Get-AdminAuditLogConfig
                     if($AuditLogsEnabled -eq $null){sleep 2; $AuditLogsEnabled = Get-AdminAuditLogConfig }
@@ -46,9 +45,7 @@ $AuditLogsEnabled = Get-AdminAuditLogConfig
                         }
 }
 
-function Get-LatestCoreProfile{
-    iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/burn56/ToolBox-Addons/master/installprofile.ps1')) 
-}
+
 
 Function wanip{
 function Write-ColorOutput
@@ -173,6 +170,10 @@ if((Test-Path $profile_file) -eq $true)
     }
 
 
+}
+function Get-LatestCoreProfile{
+    Remove-Profile
+    iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/burn56/ToolBox-Addons/master/installprofile.ps1')) 
 }
 
 function Download-Unpack-Modules{
