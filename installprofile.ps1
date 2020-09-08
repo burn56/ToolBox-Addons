cca199062aa84f8a7c3110900164849d5dadab3f
burn56 <matt.t.urbano@gmail.com>
2020-09-08 16:33:50 -0700

    Update installprofile.ps1

--- installprofile.ps1
+++ installprofile.ps1
@@ -1,39 +1,229 @@
 <#
-.Synopsis
-    Install the Coretelligent PowerShell profile.
-
+.SYNOPSIS
+Profile.ps1
 .DESCRIPTION
-    Install the Coretelligent PowerShell profile from GitHub for the desired scope.
-    By default, the existing profile will not be overwritten.
+This is the custom Profile loader to load this profile onto any machine
+.OUTPUTS
+Profile will go into the current users' documents folder
 
-.Parameter Scope
-    Name of the PowerShell profile scope to use.
-    See https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles for more information.
+.NOTES
+Written by: Matt Urbano
 
-.Parameter URL
-    Fully qualified URL to the profile.ps1 file to download.
-
-.EXAMPLE
-    Overwrite the AllUsersAllHosts PowerShell Profile with data downloaded from the URL.
-    .\install.ps1 -Scope 'AllUsersAllHosts' -URL 'http://example.com'
+Change Log
+V1.00, 09/08/20 - Initial version
 #>
-[CmdletBinding()]
-param(
-  [ValidateSet('AllUsersAllHosts', 'AllUsersCurrentHost', 'CurrentUserAllHosts', 'CurrentUserCurrentHost')]
-  [string]$Scope = "CurrentUserAllHosts",
-  [Uri]$URL = 'https://raw.githubusercontent.com/burn56/ToolBox-Addons/master/profile.ps1'
-)
-
-$profile_dir = Split-Path $PROFILE.$Scope
-$profile_file = $profile.$Scope
-$request = Invoke-WebRequest $URL -UseBasicParsing  -ContentType "text/plain; charset=utf-8"
-
-if (-not (Test-Path $profile_dir)) {
-  New-Item -Path $profile_dir -ItemType Directory | Out-Null
-  Write-Verbose "Created new profile directory: $profile_dir"
+ Param
+ (
+    [Parameter(Mandatory = $false)]
+    [switch]$reload
+    )
+$Supported = "O365-Connect (Standard Office 365 Connection)
+O365-MFAConnect (MFA O365 Connection)
+WanIP (Get Current Machine's WAN IP)
+Run-AsAdmin (Open a New Powershell Window as Admin)
+Get-LatestCoreProfile (Update the Profile with the latest version)
+Remove-Profile (To erase this profile from this Machine)"
+function CoreAuditLogCheck{
+$AuditLogsEnabled = Get-AdminAuditLogConfig
+                    if($AuditLogsEnabled -eq $null){sleep 2; $AuditLogsEnabled = Get-AdminAuditLogConfig }
+                    $AuditLogVerify = $AuditLogsEnabled.AdminAuditLogEnabled
+                    write-host "Found That Audit log enabled is set to:" -NoNewline
+                    
+                    if($AuditLogVerify -eq $true){write-host " $AuditLogVerify" -ForegroundColor Green}
+                    elseif($AuditLogVerify -eq $False)
+                        {
+                            write-host " $AuditLogVerify" -ForegroundColor Red
+                            write-host "O365 Audit Logs are detected as disabled Prompting for Enablement."
+                            $Pop = new-object -comobject wscript.shell
+		                    $AuditAnswer = $Pop.popup("Security Audit Logs are detected as not enabled.`nEnable Now?", `
+				                    0, "O365 Audit Log Status", 4)
+		                    
+		                    If ($AuditAnswer -eq 6)
+		                    {
+			                    Set-AdminAuditLogConfig -UnifiedAuditLogIngestionEnabled $true
+                                write-host "Audit Logs now enabled, it may take up to 60 minutes to take effect."
+		                    }
+                            else
+                            {
+                                write-host "User did not wish to enable O365 Audit Logs"
+                            }
+                        }
 }
 
-# Leverage IO.File for PowerShell 5 comppatability.
-[IO.File]::WriteAllLines($profile_file, $request.Content)
-Write-Verbose "Wrote profile file: $profile_file with content from: $URL"
-& $profile_file -Verbose
+
+
+Function wanip{
+function Write-ColorOutput
+{
+    [CmdletBinding()]
+    Param(
+         [Parameter(Mandatory=$False,Position=1,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][Object] $Object,
+         [Parameter(Mandatory=$False,Position=2,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][ConsoleColor] $ForegroundColor,
+         [Parameter(Mandatory=$False,Position=3,ValueFromPipeline=$True,ValueFromPipelinebyPropertyName=$True)][ConsoleColor] $BackgroundColor,
+         [Switch]$NoNewline
+    )    
+
+    # Save previous colors
+    $previousForegroundColor = $host.UI.RawUI.ForegroundColor
+    $previousBackgroundColor = $host.UI.RawUI.BackgroundColor
+
+    # Set BackgroundColor if available
+    if($BackgroundColor -ne $null)
+    { 
+       $host.UI.RawUI.BackgroundColor = $BackgroundColor
+    }
+
+    # Set $ForegroundColor if available
+    if($ForegroundColor -ne $null)
+    {
+        $host.UI.RawUI.ForegroundColor = $ForegroundColor
+    }
+
+    # Always write (if we want just a NewLine)
+    if($Object -eq $null)
+    {
+        $Object = ""
+    }
+
+    if($NoNewline)
+    {
+        [Console]::Write($Object)
+    }
+    else
+    {
+        Write-Output $Object
+    }
+
+    # Restore previous colors
+    $host.UI.RawUI.ForegroundColor = $previousForegroundColor
+    $host.UI.RawUI.BackgroundColor = $previousBackgroundColor
+}
+
+#https://ipinfo.io/ 
+Write-ColorOutput "WAN IP Information" Red
+
+$ipinfo = Invoke-RestMethod http://ipinfo.io/json 
+
+$ipinfo.ip 
+$ipinfo.hostname 
+$ipinfo.city 
+$ipinfo.region 
+$ipinfo.country 
+$ipinfo.org
+$ipinfo.name
+$ipinfo.loc
+
+
+
+} 
+function O365-Connect { 
+$UserCredential = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session -DisableNameChecking
+CoreAuditLogCheck
+}
+function Run-AsAdmin { 
+    # Get the ID and security principal of the current user account 
+    $myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent() 
+    $myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID) 
+
+    # Get the security principal for the Administrator role 
+    $adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator 
+
+    # Check to see if we are currently running "as Administrator" 
+    if ($myWindowsPrincipal.IsInRole($adminRole)) 
+    { 
+        # We are running "as Administrator" - so change the title and background color to indicate this 
+        # $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)" 
+        $Host.UI.RawUI.BackgroundColor = "DarkBlue" 
+        clear-host 
+    } 
+    else 
+    { 
+        # We are not running "as Administrator" - so relaunch as administrator 
+
+        # Create a new process object that starts PowerShell 
+        $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"; 
+
+        # Indicate that the process should be elevated 
+        $newProcess.Verb = "runas"; 
+
+        # Start the new process 
+        [System.Diagnostics.Process]::Start($newProcess); 
+
+        # Exit from the current, unelevated, process 
+        #exit 
+    } 
+
+}
+
+function Remove-Profile {
+$Scope = "CurrentUserAllHosts"
+$profile_dir = Split-Path $PROFILE.$Scope
+$profile_file = $profile.$Scope
+$module_dir = "$profile_dir\Modules"
+if((Test-Path $profile_file) -eq $true)
+    {
+        Remove-Item $profile_file -Force
+        Remove-item $module_dir -Recurse -Force -ErrorAction SilentlyContinue
+        if(test-path c:\temp\ExecutionPolicyOld.txt)
+        {
+            $OldPolicyRestore = gc c:\temp\ExecutionPolicyOld.txt
+            Set-ExecutionPolicy $OldPolicyRestore -force
+            Remove-Item c:\temp\ExecutionPolicyOld.txt
+        }
+    }
+
+
+}
+function Get-LatestCoreProfile{
+    $reload = $true
+    Remove-Profile
+    if(-not(test-path c:\temp)){ New-Item -Path c:\temp -ItemType Directory | Out-Null } ; Get-ExecutionPolicy | Out-file c:\temp\ExecutionPolicyOld.txt ; Set-ExecutionPolicy Remotesigned -force
+    iex ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/burn56/ToolBox-Addons/master/installprofile.ps1')) 
+}
+
+function Download-Unpack-Modules{
+$Scope = "CurrentUserAllHosts"
+$profile_dir = Split-Path $PROFILE.$Scope
+$module_dir = "$profile_dir\Modules"
+if(-not(test-path $module_dir))
+    {
+        New-Item -Path $module_dir -ItemType Directory | Out-Null
+        $URL = 'https://github.com/burn56/ToolBox-Addons/raw/master/CreateExoPSSession.zip'
+        $request = Invoke-WebRequest $URL -OutFile "$module_dir\ConnectEXO.zip"
+        Expand-Archive -Path "$module_dir\ConnectEXO.zip" -DestinationPath "$module_dir"
+        Remove-Item "$module_dir\ConnectEXO.zip" -Force
+        if($reload -eq $true){. $profile}
+        else{
+           $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell"; 
+           [System.Diagnostics.Process]::Start($newProcess); 
+           exit}
+    
+        
+    }
+Get-ChildItem "${module_dir}\*.ps1" -Recurse | %{.$_} 
+import-module "$module_dir\CreateExoPSSession\CreateExoPSSession.ps1"
+clear
+}
+Download-Unpack-Modules
+function O365-MFAConnect{
+$Scope = "CurrentUserAllHosts"
+$profile_dir = Split-Path $PROFILE.$Scope
+$module_dir = "$profile_dir\Modules"
+Import-Module $((Get-ChildItem -Path $($module_dir+"\CreateExoPSSession") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
+
+try{ $EXOSession = New-ExoPSSession -ErrorAction stop
+Import-PSSession $EXOSession -ErrorAction stop
+CoreAuditLogCheck}
+catch{
+Write-Host "Connection was aborted" -ForegroundColor Red}
+}
+Write-Host "Coretelligent Powershell Profile Loaded"
+Write-host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" -ForegroundColor Yellow    
+Write-Host "Current Profile Supports: " -ForegroundColor DarkCyan
+write-host "$Supported  " -ForegroundColor Green
+Write-host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"-ForegroundColor Yellow
+Write-host "Please use 'Remove-Profile' to remove this profile from the machine" -ForegroundColor Red
+Write-host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"-ForegroundColor Yellow
+
